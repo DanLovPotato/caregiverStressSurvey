2e0e99ac732d2e9ea83213fddf630f8ee9dd6412
DanLovPotato <xdantong12@gmail.com>
2024-04-23 16:02:27 -0400

    remove back bottom add submit buttom

--- frontEnd/src/pages/Q7.jsx
+++ frontEnd/src/pages/Q7.jsx
@@ -108,14 +108,6 @@ const Questionnaire = () => {
               </div>
 
               <div className="bottomButtons">
-               <button className="back-button" onClick={handleSubmit}>
-                  <span>Back</span>
-                  <svg width="15px" height="10px" viewBox="0 0 13 10">
-                      <path d="M1,5 L11,5"></path>
-                      <polyline points="8 1 12 5 8 9"></polyline>
-                  </svg>
-                </button>
-
                 <button className="next-button" onClick={handleSubmit}>
                     <span>Next</span>
                     <svg width="15px" height="10px" viewBox="0 0 13 10">
